8bafda3f613b3d27fc4a31aa5bb6e2f5baceb4ef
Bhavik Patel <bhavitk@live.in>
2021-09-01 12:36:44 +0530

    dev version

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Sterlite Application</p>
+        <p>Development version</p>
         <a
           className="App-link"
           href="https://reactjs.org"
